c3f9b0890116a06eed6a1c5cde817f48616f7d2f
Excelsies <gme97531@gmail.com>
2020-06-26 07:58:24 -0600

    Adjusted Logo and icons

--- TrackerApp/app/src/main/java/com/example/clockinouttracker/ui/home/HomeViewModel.java
+++ TrackerApp/app/src/main/java/com/example/clockinouttracker/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("You have yet to do anything.");
     }
 
     public LiveData<String> getText() {
